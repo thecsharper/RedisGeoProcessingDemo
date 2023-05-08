6d57aa7d3b775f456ca8b822ac26502566739763
thecsharper <si@thecsharper.com>
2023-05-08 11:32:33 +0100

    Added JSON cities and geo search

---
+++ RedisGeoProcessingDemo/Data/Cities.cs
@@ -0,0 +1,34 @@
+﻿using System.Text.Json.Serialization;
+
+namespace RedisGeoProcessingDemo.Data
+{
+    public class CityRecord
+    {
+        [JsonPropertyName("city")]
+        public string? City { get; set; }
+
+        [JsonPropertyName("lat")]
+        public string? Lat { get; set; }
+        
+        [JsonPropertyName("lng")]
+        public string? Lng { get; set; }
+        
+        [JsonPropertyName("country")]
+        public string? Country { get; set; }
+        
+        [JsonPropertyName("iso2")]
+        public string? Iso2 { get; set; }
+        
+        [JsonPropertyName("admin_name")]
+        public string? Admin_name { get; set; }
+        
+        [JsonPropertyName("capital")]
+        public string? Capital { get; set; }
+        
+        [JsonPropertyName("population")]
+        public string? Population { get; set; }
+        
+        [JsonPropertyName("population_proper")]
+        public string? Population_proper { get; set; }
+    }
+}
--- RedisGeoProcessingDemo/Program.cs
+++ RedisGeoProcessingDemo/Program.cs
@@ -1,2 +1,38 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using RedisGeoProcessingDemo.Data;
+using StackExchange.Redis;
+using System.Text.Json;
+
+var fileName = "D:\\Develop\\RedisGeoProcessingDemo\\gb.json";
+
+//using FileStream openStream = File.OpenRead(fileName);
+//var cities = await JsonSerializer.DeserializeAsync<CityRecord[]>(openStream);
+
+var options = ConfigurationOptions.Parse("localhost:6379");
+options.Password = "eYVX7EwVmmxKPCDmwMtyKVge8oLd2t81";
+
+var redis = ConnectionMultiplexer.Connect(options);
+
+var db = redis.GetDatabase();
+
+var key = "1"; 
+var value = "A";
+
+db.StringSet(key, value);
+
+value = db.StringGet(key);
+
+Console.WriteLine($"Key value: {value}");
+
+//foreach (var city in cities!)
+//{
+//    var geoEntry = new GeoEntry(Convert.ToDouble(city.Lat), Convert.ToDouble(city.Lng), city.City);
+
+//    await db.GeoAddAsync("UK", geoEntry);
+
+//   Console.WriteLine($"{city.City} {city.Lat} {city.Lng}");
+//}
+
+Console.WriteLine(await db.GeoDistanceAsync("UK", "Bristol", "Charfield", GeoUnit.Miles));
+Console.WriteLine(await db.GeoDistanceAsync("UK", "Bristol", "Alford", GeoUnit.Miles));
+
+Console.WriteLine(await db.GeoSearchAsync("UK", );
