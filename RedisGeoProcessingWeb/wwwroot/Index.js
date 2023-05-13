943eac68eeeabe05f06c82f58c20c00c76887ca5
thecsharper <si@thecsharper.com>
2023-05-13 19:54:02 +0100

    Refactor to JS

--- RedisGeoProcessingWeb/wwwroot/Index.js
+++ RedisGeoProcessingWeb/wwwroot/Index.js
@@ -2,20 +2,17 @@
 let map;
 
 async function initMap() {
-    // The location of Uluru
-    const position = { lat: -25.344, lng: 131.031 };
-    // Request needed libraries.
+
+    const position = { lat: 51.454514, lng: -2.58 };
+
     //@ts-ignore
     const { Map } = await google.maps.importLibrary("maps");
-    //const { AdvancedMarkerView } = await google.maps.importLibrary("marker");
 
-    // The map, centered at Uluru
     map = new Map(document.getElementById("map"), {
-        zoom: 4,
+        zoom: 10,
         center: position,
         mapId: "DEMO_MAP_ID",
     });
-
 
     new google.maps.Marker({
         position: position,
@@ -23,41 +20,20 @@ async function initMap() {
         title: "Hello World!",
     });
 
-    // The marker, positioned at Uluru
-    //const marker = new AdvancedMarkerView({
-    //    map: map,
-    //    position: position,
-    //    title: "Uluru",
-    //});
-
-    var myLatlng = new google.maps.LatLng(51.454514, -2.58);
-    //var mapOptions = {
-    //    zoom: 4,
-    //    center: myLatlng
-    //}
-    //var map = new google.maps.Map(document.getElementById("map"), mapOptions);
-
     $.getJSON("https://localhost:32419/", function (data) {
         var items = [];
         $.each(data, function (key, val) {
             items.push(val);
         });
         $(
-
             items.forEach(function (number) {
                 new google.maps.Marker({
                     position: new google.maps.LatLng(number.position.longitude, number.position.latitude),
                     title: String(number.distance) 
                 }).setMap(map);
             })
-
-
-        )
+         )
     });
-
-    // To add the marker to the map, call setMap();
-    //marker.setMap(map);
 }
 
 initMap();
-
