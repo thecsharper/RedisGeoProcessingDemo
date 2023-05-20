1fab5ce85cdb908cc58cb53257b3c27a73328d64
thecsharper <si@thecsharper.com>
2023-05-20 15:50:12 +0100

    Added test setup

---
+++ RedisGeoProcessingDemoTests/AddDataToRedisTests.cs
@@ -0,0 +1,17 @@
+using Moq;
+using FluentAssertions;
+using StackExchange.Redis;
+
+namespace RedisGeoProcessingDemoTests
+{
+    public class AddDataToRedisTests
+    {
+        private readonly Mock<IDatabase>? _database;
+
+        [Fact]
+        public void Test1()
+        {
+            
+        }
+    }
+}
