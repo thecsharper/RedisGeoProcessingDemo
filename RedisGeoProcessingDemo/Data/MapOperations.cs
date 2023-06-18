ff70d894fe2c5a7d7ac5d30f3779b33625bc4c2a
thecsharper <si@thecsharper.com>
2023-06-18 21:34:42 +0100

    Added map popups

--- RedisGeoProcessingDemo/Data/MapOperations.cs
+++ RedisGeoProcessingDemo/Data/MapOperations.cs
@@ -8,7 +8,7 @@ namespace RedisGeoProcessingDemo.Data
         {
             var geoResults = await database.GeoSearchAsync("UK", Convert.ToDouble("54.0840"), Convert.ToDouble("2.8594"), new GeoSearchCircle(Convert.ToDouble("350"), GeoUnit.Miles));
 
-            var places = new List<string>();
+            var placeItem = new List<string>();
 
             foreach (var geoResult in geoResults.ToList())
             {
@@ -21,15 +21,16 @@ namespace RedisGeoProcessingDemo.Data
                         Lng = geoResult.Position!.Value.Longitude
                     };
 
-                    places.Add($"<li onClick = \"selectCountry('{place.City}');selectMap($(this).data('lat'),$(this).data('lng'));\" data-lat=\"{place.Lat}\" data-lng=\"{place.Lng}\">{place.City}</li>");
+                    placeItem.Add($"<li onClick = \"selectCountry('{place.City}');selectMap($(this).data('lat'),$(this).data('lng'));\" data-lat=\"{place.Lat}\" data-lng=\"{place.Lng}\">{place.City}</li>");
                 }
             }
 
-            return places;
+            return placeItem;
         }
 
        public async Task<GeoRadiusResult[]> GetGeoResults(IDatabase database, string? lat, string? lng)
         {
+            // Default location
             if (string.IsNullOrEmpty(lat) || string.IsNullOrEmpty(lng))
             {
                 lat = "51.454514";
