099097feef54071c64d953b6eec65b5ec9dcd890
thecsharper <si@thecsharper.com>
2023-06-24 08:29:36 +0100

    Added map operations tests

---
+++ RedisGeoProcessingDemoTests/MapOperationsTests.cs
@@ -0,0 +1,28 @@
+﻿using Moq;
+using StackExchange.Redis;
+using RedisGeoProcessingDemo.Data;
+
+namespace RedisGeoProcessingDemoTests
+{
+    public  class MapOperationsTests
+    {
+            private Mock<IDatabase>? _database;
+
+        [Fact]
+        public async Task Get_all_places()
+        {
+
+             GeoRadiusResult[] geoRadiusResults = { new GeoRadiusResult() { } };
+            
+            _database = new Mock<IDatabase>();
+
+            _database!
+                .Setup(x => x.GeoSearchAsync("UK", Convert.ToDouble("54.0840"), Convert.ToDouble("2.8594"), new GeoSearchCircle(Convert.ToDouble("350"), GeoUnit.Miles)))
+                .ReturnsAsync(geoRadiusResults);
+                
+            await new MapOperations().GetAllPlaces(_database!.Object, "Place");
+
+           // _database.Verify(x => x.GeoAddAsync(It.IsAny<RedisKey>(), It.IsAny<GeoEntry>(), It.IsAny<CommandFlags>()), Times.Once());
+        }
+    }
+}
