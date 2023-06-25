3df32ce7973537e54ab708435380b7b460a65d3c
thecsharper <si@thecsharper.com>
2023-06-25 11:57:27 +0100

    Added map test

--- RedisGeoProcessingDemoTests/MapOperationsTests.cs
+++ RedisGeoProcessingDemoTests/MapOperationsTests.cs
@@ -16,13 +16,35 @@ namespace RedisGeoProcessingDemoTests
             
             _database = new Mock<IDatabase>();
 
-            _database!
-                .Setup(x => x.GeoSearchAsync("UK", Convert.ToDouble("54.0840"), Convert.ToDouble("2.8594"), new GeoSearchCircle(Convert.ToDouble("350"), GeoUnit.Miles)))
-                .ReturnsAsync(geoRadiusResults);
-                
+            _database.Setup(x => x.GeoSearchAsync(It.IsAny<RedisKey>(), It.IsAny<RedisValue>(), It.IsAny<GeoSearchShape>(), It.IsAny<int>(), It.IsAny<bool>(), It.IsAny<Order>(), It.IsAny<GeoRadiusOptions>(), It.IsAny<CommandFlags>())).Returns(Task.FromResult(geoRadiusResults));
+
             await new MapOperations().GetAllPlaces(_database!.Object, "Place");
 
-           // _database.Verify(x => x.GeoAddAsync(It.IsAny<RedisKey>(), It.IsAny<GeoEntry>(), It.IsAny<CommandFlags>()), Times.Once());
+            _database.Verify(x => x.GeoSearchAsync(It.IsAny<RedisKey>(), It.IsAny<RedisValue>(), It.IsAny<GeoSearchShape>(), It.IsAny<int>(), It.IsAny<bool>(), It.IsAny<Order>(), It.IsAny<GeoRadiusOptions>(), It.IsAny<CommandFlags>()), Times.Once());
         }
     }
 }
+
+
+//// Assuming you have a Redis client interface with the GeoSearchAsync method
+//public interface IRedisClient
+//{
+//    Task<string[]> GeoSearchAsync(string key, double longitude, double latitude, double radius);
+//}
+
+//// Creating a mock instance of the Redis client
+//var redisMock = new Mock<IRedisClient>();
+
+//// Setting up the mock behavior for the GeoSearchAsync method
+//redisMock.Setup(client => client.GeoSearchAsync(It.IsAny<string>(), It.IsAny<double>(), It.IsAny<double>(), It.IsAny<double>()))
+//    .ReturnsAsync(new string[] { "Location1", "Location2", "Location3" });
+
+//// Using the mock in your code
+//IRedisClient redisClient = redisMock.Object;
+//string[] locations = await redisClient.GeoSearchAsync("myKey", 12.34, 56.78, 1000);
+
+//// Asserting the expected results
+//Assert.Equal(3, locations.Length);
+//Assert.Contains("Location1", locations);
+//Assert.Contains("Location2", locations);
+//Assert.Contains("Location3", locations);
