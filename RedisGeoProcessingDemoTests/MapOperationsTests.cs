d912faf197cf37d135303d4be62a7dbd3136e2b6
thecsharper <si@thecsharper.com>
2023-06-27 22:15:26 +0100

    Added tests

--- RedisGeoProcessingDemoTests/MapOperationsTests.cs
+++ RedisGeoProcessingDemoTests/MapOperationsTests.cs
@@ -26,5 +26,23 @@ namespace RedisGeoProcessingDemoTests
 
             result.Should().BeOfType<List<string>>();
         }
+
+        [Fact]
+        public async Task Get_geo_results()
+        {
+            var postion = new GeoPosition(32.3, 1.11);
+
+            var geoResult = new GeoRadiusResult("UK", 3.23, 0, postion);
+
+            GeoRadiusResult[] geoRadiusResults = { geoResult };
+
+            _database = new Mock<IDatabase>();
+
+            _database.Setup(x => x.GeoSearchAsync(It.IsAny<RedisKey>(), It.IsAny<RedisValue>(), It.IsAny<GeoSearchShape>(), It.IsAny<int>(), It.IsAny<bool>(), It.IsAny<Order>(), It.IsAny<GeoRadiusOptions>(), It.IsAny<CommandFlags>())).Returns(Task.FromResult(geoRadiusResults));
+
+            var result = await new MapOperations().GetGeoResults(_database!.Object, "1.1","-0.1");
+
+            result.Should().BeOfType<List<Places>>();
+        }
     }
 }
