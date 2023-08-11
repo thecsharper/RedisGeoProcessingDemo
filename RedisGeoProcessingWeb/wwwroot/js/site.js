0ce681ffcc39d683361941ce65c81162b5013337
thecsharper <si@thecsharper.com>
2023-08-11 12:43:05 +0100

    Added JS test project

--- RedisGeoProcessingWeb/wwwroot/js/site.js
+++ RedisGeoProcessingWeb/wwwroot/js/site.js
@@ -173,6 +173,7 @@ $(document).ready(function () {
         });
     });
 });
+
 function calculateAndDisplayRoute(directionsService, directionsRenderer) {
     const waypts = [];
     const checkboxArray = document.getElementById("waypoints");
